ee68ddf2c2b39bf11dd1717f4ec0e52e72b362ac
DaniilSukhovskoy <57703685+DaniilSukhovskoy@users.noreply.github.com>
2022-05-18 13:16:33 +0300

    Add mobile navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { Burger } from "../Burger/Burger";
+import { useState, useRef } from "react";
+import { useOnClickOutside } from "../../services/hooks";
 
 export default function Header() {
+    const [open, setOpen] = useState(false);
+    const node = useRef(); 
+    useOnClickOutside(node, () => setOpen(false));
+
+
+
     return (
-        <header className="container">
-            <div>
-                <h1>Daniil Sukhovskoy</h1>
-            </div>
-            <nav>
+        <header
+            className={!open ? "" : "open"}
+            ref={node}
+        >
+            <h1>Daniil Sukhovskoy</h1>
+            <nav
+                onClick={() => setOpen(false)}
+            >
                 <ul>
                     <li><NavLink to="/work">Work</NavLink></li>
                     <li><NavLink to="/personal">Personal</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                 </ul>
             </nav>
+            <Burger
+                open={open}
+                setOpen={setOpen}
+            />
         </header>
     )
 }
